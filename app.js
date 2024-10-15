27c3b11db73915b7f9f752299e7c944b5d8c2d58
Loreina Salalila <vialoraine.gg@gmail.com>
2024-10-15 00:55:21 -0700

    task 6 completed

--- app.js
+++ app.js
@@ -4,4 +4,5 @@ const kelvin = 293;
 //Temperature in Celsius (conversion from Kelvin)
 let celsius = kelvin - 273;
 
+//Temperature in Fahrenheit (conversion from Celsius)
 let fahrenheit = celsius * (9/5) + 32;
