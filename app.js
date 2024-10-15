2ddf366a484fc02348168644371fb51f59452127
Loreina Salalila <vialoraine.gg@gmail.com>
2024-10-15 01:02:46 -0700

    task 9 completed

--- app.js
+++ app.js
@@ -7,3 +7,9 @@ let celsius = kelvin - 273;
 //Temperature in Fahrenheit (conversion from Celsius)
 //Used Math.floor() method to round number to whole number
 let fahrenheit = Math.floor(celsius * (9/5) + 32);
+
+//Answers
+console.log(`The temperature is ${kelvin} degrees Kelvin.`)
+console.log(`The temperature is ${celsius} degrees Celsius.`)
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
+console.log(`The temperature is ${newton} degrees Newton.`)
