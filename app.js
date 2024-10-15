a9c1a606e3e1dfae50a30deaecc350109ab9d4b5
Loreina Salalila <vialoraine.gg@gmail.com>
2024-10-15 00:57:14 -0700

    task 7 completed

--- app.js
+++ app.js
@@ -5,5 +5,4 @@ const kelvin = 293;
 let celsius = kelvin - 273;
 
 //Temperature in Fahrenheit (conversion from Celsius)
-//Used Math.floor() method to round number to whole number
 let fahrenheit = Math.floor(celsius * (9/5) + 32);
